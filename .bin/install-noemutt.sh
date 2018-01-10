7ee9b2fea5f040cf7f03fa3e8badc40b9d8ba04c
Alizée Penel <alizee@lse.epita.fr>
2018-01-10 17:02:48 +0100

    {neomutt}[kynzie] Change labels with whitespace
    Signed-off-by: Alizée Penel <alizee@lse.epita.fr>

--- .bin/install-noemutt.sh
+++ .bin/install-noemutt.sh
@@ -3,34 +3,34 @@
 CMD="notmuch config set query."
 echo "Create notmuch queries for neomutt virtual mailbox"
 
-${CMD}lse-mls "tag:\"EPITA/LSE/ML current\" or tag:\"EPITA/LSE/ML permanents\""
+${CMD}lse-mls "tag:EPITA/LSE/ML-current or tag:EPITA/LSE/ML-permanents"
 
 ${CMD}acu-yaka "tag:EPITA/ACU-YAKA or tag:EPITA/ACU-YAKA/Gitlab"
 
-${CMD}lec-prep	"tag:\"EPITA/Lectures preparation\" or \
-		tag:\"EPITA/Lectures preparation/SAND\""
+${CMD}lec-prep	"tag:EPITA/LecturesPreparation or \
+		tag:EPITA/LecturesPreparation/SAND"
 
 ${CMD}family	"tag:tag:Family/Loche or tag:Family/Mignot or tag:Family/Penel"
 
 ${CMD}friends	"tag:Friends or tag:Friends/Dogs or Friends/EPITA or 	\
-		Friends/JDR-GN or tag:\"Friends/Prepa class\" or	\
+		Friends/JDR-GN or tag:Friends/PrepaClass or	\
 		tag:Friends/sjbranleurs"
 
-${CMD}lkml	"tag:KERNEL or tag:\"KERNEL/Kernel newbies\"		\
-		tag:KERNEL\LKML or tag:\"KERNEL/Linux embedded\""
+${CMD}lkml	"tag:KERNEL or tag:KERNEL/KernelNewbies		\
+		tag:KERNEL/LKML or tag:KERNEL/LinuxEmbedded"
 
-${CMD}bank	"tag:\"Personal management/Bank\" or 			\
-		tag:\"Personal management/Bank/SG\""
+${CMD}bank	"tag:PersonalManagement/Bank or 			\
+		tag:PersonalManagement/Bank/SG"
 
-${CMD}flat	"tag:\"Personal management/Flat\" or 			\
-		tag:\"Personal management/Flat/EDF-GDF\" or	\
-		tag:\"Personal management/Flat/Insurances\" or	\
-		tag:\"Personal management/Flat/Rent\""
+${CMD}flat	"tag:PersonalManagement/Flat or 			\
+		tag:PersonalManagement/Flat/EDF-GDF or	\
+		tag:PersonalManagement/Flat/Insurances or	\
+		tag:PersonalManagement/Flat/Rent"
 
-${CMD}internet-phone	"tag:\"Personal management/Cellphone\" or	\
-			tag:\"Personal management/Cellphone/Free\" or	\
-			tag:\"Personal management/Flat/Internet\" or	\
-			tag:\"Personal management/Flat/Internet/SFR\""
+${CMD}internet-phone	"tag:PersonalManagement/Cellphone or	\
+			tag:PersonalManagement/Cellphone/Free or	\
+			tag:PersonalManagement/Flat/Internet or	\
+			tag:PersonalManagement/Flat/Internet/SFR"
 
 
 ${CMD}shopping	"tag:E-shops or tag:E-shops/Alixexpress or 		\
@@ -38,16 +38,16 @@ ${CMD}shopping	"tag:E-shops or tag:E-shops/Alixexpress or 		\
 		tag:E-shops/CDiscount or tag:E-shops/Ebay or 		\
 		tag:E-shops/Paypal or tag:E-shops/PriceMinister or 	\
 		tag:E-shops/RueDuCommerce or tag:E-shops/Uber or 	\
-		tag:\"E-shops/Video games\""
+		tag:E-shops/VideoGames"
 
 ${CMD}homelab	"tag:Homelab or tag:Homelab/Services"
 
-${CMD}perso-others	"tag:\"Personal management/Employee savings\" or
-			tag:\"Personal management/Tignes\""
+${CMD}perso-others	"tag:PersonalManagement/EmployeeSavings or
+			tag:PersonalManagement/Tignes"
 
 ${CMD}studies-epita-adm	"tag:Studies/EPITA/ADM or 			\
 			tag:Studies/EPITA/Business or	\
-			tag:Studies/EPITA/Com' or	\
+			tag:Studies/EPITA/Com or	\
 			tag:Studies/EPITA/Intranet or	\
 			tag:Studies/EPITA/MSDNA"
 
@@ -73,7 +73,7 @@ ${CMD}project	"tag:Studies/EPITA/ING1/Projects or 			\
 		tag:Studies/EPITA/ING1/Projects/JDV or			\
 		tag:Studies/EPITA/ING1/Projects/K or 			\
 		tag:Studies/EPITA/ING1/Projects/Piscine or		\
-		tag:Studies/EPITA/ING1/Projects/RushJeuVideo\" or \
+		tag:Studies/EPITA/ING1/Projects/RushJeuVideo or \
 		tag:Studies/EPITA/ING1/Projects/Tiger or		\
 		tag:Studies/EPITA/ING1/Projects/YakHospital or		\
 		tag:Studies/EPITA/ING1/Projects/Zik-end or		\
@@ -85,32 +85,32 @@ ${CMD}project	"tag:Studies/EPITA/ING1/Projects or 			\
 		tag:Studies/EPITA/ING1/Projects/myReadISO"
 
 ${CMD}work	"tag:Work or tag:Work/Genymobile or 			\
-		tag:Work/Genymobile/PaySlips or tag:\"Work/Ionis Tutoring\" or \
-		tag:\"Work/Ionis Tutoring/Students\" or			\
+		tag:Work/Genymobile/PaySlips or tag:Work/IonisTutoring or \
+		tag:Work/IonisTutoring/Students or			\
 		tag:Studies/EPITA/Business/Interships/Offers or 	\
 		tag:Work/Playdigious or tag:Work/Thales"
 
-${CMD}perso-old	"tag:\"Personal management/Car\" or			\
-		tag:\"Personal management/Flat/Internet/Free\" or	\
-		tag:\"Personal management/Cellphone/SFR\" or	\
-		tag:\"Personal management/Flat/CAF\""
+${CMD}perso-old	"tag:PersonalManagement/Car or			\
+		tag:PersonalManagement/Flat/Internet/Free or	\
+		tag:PersonalManagement/Cellphone/SFR or	\
+		tag:PersonalManagement/Flat/CAF"
 
-${CMD}epita-old	"tag:\"EPITA/Finished missions\" or 			\
-		tag:\"EPITA/Finished missions/Apprentices-2017\" or	\
-		tag:\"EPITA/Finished missions/Apprentices-2017/Engineers\" or \
-		tag:\"EPITA/Finished missions/Apprentices-2017/Experts\" or \
-		tag:\"EPITA/Finished missions/Apprentices-2018\" or	\
-		tag:\"EPITA/Finished missions/Apprentices-2018/Engineers\" or \
-		tag:\"EPITA/Finished missions/Apprentices-2018/Experts\" or   \
-		tag:\"EPITA/Finished missions/Apprentices-2019\" or	\
-		tag:\"EPITA/Finished missions/Apprentices-2019/Engineers\" or \
-		tag:\"EPITA/Finished missions/Apprentices-2019/Experts\" or   \
-		tag:\"EPITA/Finished missions/Cumulatifs\" or		\
-		tag:\"EPITA/Finished missions/Evaluation book\" or	\
-		tag:\"EPITA/Finished missions/ING1 exams proofreading\" or    \
-		tag:\"EPITA/Finished missions/SAND-2015\" or		\
-		tag:\"EPITA/Finished missions/SAND-2016\" or		\
-		tag:\"EPITA/Finished missions/SAND-2017\""
+${CMD}epita-old	"tag:EPITA/FinishedMissions or 			\
+		tag:EPITA/FinishedMissions/Apprentices-2017 or	\
+		tag:EPITA/FinishedMissions/Apprentices-2017/Engineers or \
+		tag:EPITA/FinishedMissions/Apprentices-2017/Experts or \
+		tag:EPITA/FinishedMissions/Apprentices-2018 or	\
+		tag:EPITA/FinishedMissions/Apprentices-2018/Engineers or \
+		tag:EPITA/FinishedMissions/Apprentices-2018/Experts or   \
+		tag:EPITA/FinishedMissions/Apprentices-2019 or	\
+		tag:EPITA/FinishedMissions/Apprentices-2019/Engineers or \
+		tag:EPITA/FinishedMissions/Apprentices-2019/Experts or   \
+		tag:EPITA/FinishedMissions/Cumulatifs or		\
+		tag:EPITA/FinishedMissions/EvaluationBook or	\
+		tag:EPITA/FinishedMissions/ING1ExamsProofreading or    \
+		tag:EPITA/FinishedMissions/SAND-2015 or		\
+		tag:EPITA/FinishedMissions/SAND-2016 or		\
+		tag:EPITA/FinishedMissions/SAND-2017"
 
 ${CMD}web-accounts	"tag:Web or tag:Web/Facebook or tag:Web/Twitter or \
 			tag:Web/Others "
